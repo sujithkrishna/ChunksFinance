c63ab7639834f13d5f49aef740cbf16442386802
Sujith Krishna <Sujith Krishna@192.168.1.116>
2025-02-18 18:37:32 -0600

    Blank Project- Chunks Finance

---
+++ src/test/java/com/finance/ChunksFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunksFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
